a156756e3e13ce1e252a8c23bb6b2a76f75742f9
Mrunal Pagnis <mrunalpagnis@yahoo.in>
2015-11-28 12:18:53 -0500

    tasks in comment

--- src/TestConnection.java
+++ src/TestConnection.java
@@ -10,7 +10,13 @@ public class TestConnection {
 			DBCollection intervaldata = db.getCollection("intervaldata");
 			int count = intervaldata.find().count();
 			System.out.println(count);
-			
+			// remove all other stops which are not major stops 
+			// find unique bus numbers
+			// for each bus number 
+				// calculate time lapse from the next major stop
+				// store diff in map
+			// calculate deviation by comparing diff in map with planned schedule
+			// calculate total deviation for each round trip and store
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
